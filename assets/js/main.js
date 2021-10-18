fde7b28eb7c6d0b17a6aee7c57ea1632b946160d
khalilannbiya <syeichkhalil30@gmail.com>
2021-10-18 19:40:56 +0700

    menambahkan file

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+(function ($) {
+  "use strict";
+
+  // Preloader
+  $(window).on("load", function () {
+    if ($("#preloader").length) {
+      $("#preloader")
+        .delay(100)
+        .fadeOut("slow", function () {
+          $(this).remove();
+        });
+    }
+  });
+})(jQuery);
