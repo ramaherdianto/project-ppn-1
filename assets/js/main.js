dfb112df598f4b01fe8b8951035acc5862cc5c84
khalilannbiya <syeichkhalil30@gmail.com>
2021-10-26 06:01:57 +0700

    menambahkan splide di our clients

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,22 @@ new Splide(".splide", {
   autoplay: true,
   interval: 3000,
 }).mount();
+
+// ==========Splide-client
+
+new Splide(".splide-client", {
+  type: "loop",
+  perPage: 3,
+  breakpoints: {
+    429: {
+      perPage: 2,
+      fixedHeight: "40vw",
+    },
+  },
+  fixedHeight: "28vw",
+  width: "80%",
+  gap: "2vw",
+  pagination: false,
+  autoplay: true,
+  interval: 3000,
+}).mount();
